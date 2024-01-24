b44f580d0c3881077ca7e45108b4158e57570f46
DeadM00n <velikanov-em@yandex.ru>
2024-01-24 19:07:20 +0300

    Added print function

--- MainClassTable.cpp
+++ MainClassTable.cpp
@@ -22,17 +22,23 @@ void MainClassTable::generateClassTable(const std::string &classname_)
     m_mainTypeID = addOrConfirmUtf8ToTable("([Ljava/lang/String;)V");
     m_codeAttrNameID = addOrConfirmUtf8ToTable("Code");
 
-    auto fldName = addOrConfirmUtf8ToTable("nobj");
-    auto fldDesc = addOrConfirmUtf8ToTable("LDynamicType;");
-    m_fldref = addOrConfirmFieldRefToTable("nobj", "LDynamicType;", classname_);
+    m_dtClass = addOrConfirmClassToTable("DynamicType");
+    m_dtInitIdI = addOrConfirmMethodRefToTable("<init>", "(I)V", "DynamicType");
+    m_dtInitIdD = addOrConfirmMethodRefToTable("<init>", "(D)V", "DynamicType");
+    m_dtInitIdS = addOrConfirmMethodRefToTable("<init>", "(Ljava/lang/String;)V", "DynamicType");
+    m_dtInitIdF = addOrConfirmMethodRefToTable("<init>", "(LFunctionClass;)V", "DynamicType");
+    m_dtInitIdNIL = addOrConfirmMethodRefToTable("<init>", "()V", "DynamicType");
+    m_dtFieldIdI = addOrConfirmFieldRefToTable("iValue", "I", "DynamicType");
+    m_dtFieldIdD = addOrConfirmFieldRefToTable("dValue", "D", "DynamicType");
+    m_dtFieldIdS = addOrConfirmFieldRefToTable("sValue", "Ljava/lang/String;", "DynamicType");
+    m_dtFieldIdF = addOrConfirmFieldRefToTable("fValue", "LFunctionClass;", "DynamicType");
 
-    auto dtclass = addOrConfirmClassToTable("DynamicType");
-    auto dtinitid = addOrConfirmMethodRefToTable("<init>", "(I)V", "DynamicType");
-    auto dtfieldid = addOrConfirmFieldRefToTable("iValue", "I", "DynamicType");
-
-    auto func1class = addOrConfirmClassToTable("FunctionClass1");
-    auto func1call = addOrConfirmMethodRefToTable("function", "()V", "FunctionClass1");
-    auto func1init = addOrConfirmMethodRefToTable("<init>", "()V", "FunctionClass1");
+    m_dtCallRef = addOrConfirmMethodRefToTable("__call", "(LDynamicType;LVarList;)LVarList;", "DynamicType");
+    
+    auto varlistClass = addOrConfirmClassToTable("VarList");
+    auto varlistInit = addOrConfirmMethodRefToTable("<init>", "()V", "VarList");
+    auto varlistAdd = addOrConfirmMethodRefToTable("add", "(LDynamicType;)V", "VarList");
+    auto varlistGet = addOrConfirmMethodRefToTable("get", "(I)LDynamicType;", "VarList");
 
     // =========== MAIN =================
     auto *mainmethod = new MethodInfo();
@@ -41,31 +47,83 @@ void MainClassTable::generateClassTable(const std::string &classname_)
     mainmethod->m_descIndex = m_mainTypeID;
     mainmethod->m_attribCount = 1;
     mainmethod->m_codeAttrNameIndex = m_codeAttrNameID;
-    mainmethod->m_maxStack = 3;
+    mainmethod->m_maxStack = 10;
     mainmethod->m_maxLocals = 1;
 
-    /* Initialize DynamicType and put into field
+    // === Initialize DynamicType with print function and put into field
+    auto printdata = createFunctionField(mainmethod, "print", classname_);
+    
+    // === Call print function
+
+    // Put DynamicType from static into stack
+    mainmethod->addBytes(0xb2, 1); // getstatic
+    mainmethod->addBytes(printdata.m_fieldRefID, 2); // print field
+    
+    // Create and initialize VarList
     mainmethod->addBytes(0xbb, 1); // new
-    mainmethod->addBytes(dtclass, 2); // DynamicType
+    mainmethod->addBytes(varlistClass, 2); // VarList
+    mainmethod->addBytes(0x59, 1); // dup
+    mainmethod->addBytes(0xb7, 1); // invokespecial
+    mainmethod->addBytes(varlistInit, 2); // <init>
+
+    //Duplicate varlist for add
+    mainmethod->addBytes(0x59, 1); // dup
+
+    // Create DT for VarList
+    mainmethod->addBytes(0xbb, 1); // new
+    mainmethod->addBytes(m_dtClass, 2); // VarList
     mainmethod->addBytes(0x59, 1); // dup
     mainmethod->addBytes(0x2, 1); // iconst_m1
     mainmethod->addBytes(0xb7, 1); // invokespecial
-    mainmethod->addBytes(dtinitid, 2); // <init>
-    mainmethod->addBytes(0xb3, 1); // putstatic
-    mainmethod->addBytes(m_fldref, 2); // nobj field
-    */
-    mainmethod->addBytes(0xbb, 1); // new
-    mainmethod->addBytes(func1class, 2); // FunctionClass1
-    mainmethod->addBytes(0x59, 1); // dup
-    mainmethod->addBytes(0xb7, 1); // invokespecial
-    mainmethod->addBytes(func1init, 2); // <init>
-    mainmethod->addBytes(0xb6, 1); // Invoke virtual
-    mainmethod->addBytes(func1call, 2); // FunctionClass1 function
+    mainmethod->addBytes(m_dtInitIdI, 2); // <init>
+
+    // Call VarList add
+    mainmethod->addBytes(0xb6, 1); // invokevirtual
+    mainmethod->addBytes(varlistAdd, 2); // add
+
+    // Call print
+    mainmethod->addBytes(0xb8, 1); // invokestatic
+    mainmethod->addBytes(m_dtCallRef, 2); // __call
+
+    // return
     mainmethod->addBytes(0xb1, 1);
+
     mainmethod->m_codeLength = mainmethod->m_byteCode.size();
     mainmethod->m_codeAttrLength = mainmethod->m_codeLength + 12;
 
     m_methodPool.push_back(mainmethod);
+}
+
+FieldData MainClassTable::createFunctionField(MethodInfo *method, const std::string &functionName, const std::string &className)
+{
+    auto fldName = addOrConfirmUtf8ToTable(functionName);
+    auto fldDesc = addOrConfirmUtf8ToTable("LDynamicType;");
+    auto fldRef = addOrConfirmFieldRefToTable(functionName, "LDynamicType;", className);
+
+    auto functionClass = addOrConfirmClassToTable(functionName);
+    //auto functionCall = addOrConfirmMethodRefToTable("function", "(LVarList;)LVarList;", "print");
+    auto fuhctionInit = addOrConfirmMethodRefToTable("<init>", "()V", functionName);
+
+    // Create DynamicType
+    method->addBytes(0xbb, 1); // new
+    method->addBytes(m_dtClass, 2); // DynamicType
+    method->addBytes(0x59, 1); // dup
+    
+    // Create and initialize function class
+    method->addBytes(0xbb, 1); // new
+    method->addBytes(functionClass, 2); // FunctionClass
+    method->addBytes(0x59, 1); // dup
+    method->addBytes(0xb7, 1); // invokespecial
+    method->addBytes(fuhctionInit, 2); // <init>
+
+    // Initialize DynamicType
+    method->addBytes(0xb7, 1); // invokespecial
+    method->addBytes(m_dtInitIdF, 2); // <init>
+
+    // Put function into static field
+    method->addBytes(0xb3, 1); // putstatic
+    method->addBytes(fldRef, 2); // print field
+
 
     auto *mainfield = new FieldInfo();
     mainfield->m_accessFlags = 0x0009;
@@ -73,7 +131,6 @@ void MainClassTable::generateClassTable(const std::string &classname_)
     mainfield->m_descIndex = fldDesc;
     m_fieldPool.push_back(mainfield);
 
-    FunctionClassTable funcclass;
-    funcclass.generateClassTable("FunctionClass1");
-    funcclass.generateClassFile();
+    return {fldName, fldDesc, fldRef};
+
 }
--- MainClassTable.h
+++ MainClassTable.h
@@ -3,6 +3,13 @@
 #include "ClassTable.h"
 #include "FunctionClassTable.h"
 
+struct FieldData
+{
+    size_t m_fieldNameID;
+    size_t m_fieldDescID;
+    size_t m_fieldRefID;
+};
+
 class MainClassTable : public ClassTable
 {
 public:
@@ -11,6 +18,8 @@ public:
 
 
 protected:
+    FieldData createFunctionField(MethodInfo *method, const std::string &functionName, const std::string &className);
+
     size_t m_javaLangSystemID = 1;
     size_t m_javaIOPrintStreamID = 1;
     size_t m_outFieldID = 1;
@@ -19,8 +28,19 @@ protected:
     size_t m_mainNameID = 1;
     size_t m_mainTypeID = 1;
     size_t m_codeAttrNameID = 1;
-    size_t m_fldref = 1;
     size_t m_placeholderStr = 1;
+
+    size_t m_dtClass = 1;
+    size_t m_dtInitIdI = 1;
+    size_t m_dtInitIdD = 1;
+    size_t m_dtInitIdS = 1;
+    size_t m_dtInitIdF = 1;
+    size_t m_dtInitIdNIL = 1;
+    size_t m_dtFieldIdI = 1;
+    size_t m_dtFieldIdD = 1;
+    size_t m_dtFieldIdS = 1;
+    size_t m_dtFieldIdF = 1;
+    size_t m_dtCallRef = 1;
 };
 
 #endif
